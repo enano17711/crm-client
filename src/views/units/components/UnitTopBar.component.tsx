b10a0e980a4c452d18924d36a4ac90d6142bcf2c
enano17711 <enano17711@gmail.com>
2023-10-11 17:50:01 -0400

    unit topbar ok. UnitTopBar.component.tsx

---
+++ src/views/units/components/UnitTopBar.component.tsx
@@ -0,0 +1,74 @@
+import React, { useCallback } from "react"
+import { useAtom, useSetAtom } from "jotai"
+import {
+   openUnitDeleteModalAtom,
+   selectedUnitAtom,
+   unitGridColumnsVisibleAtom,
+} from "../../../store/unit.atoms.ts"
+import { useNavigate } from "react-router-dom"
+import { Checkbox, Group } from "@mantine/core"
+import ActionCreateComponent from "../../../components/top-bar/ActionCreate.component.tsx"
+import ActionCloneComponent from "../../../components/top-bar/ActionClone.component.tsx"
+import ActionEditComponent from "../../../components/top-bar/ActionEdit.component.tsx"
+import ActionDeleteComponent from "../../../components/top-bar/ActionDelete.component.tsx"
+import SearchUnitByColumnComponent from "../../units/components/SearchUnitByColumn.component.tsx"
+import ActionExportComponent from "../../../components/top-bar/ActionExport.component.tsx"
+import ActionColumnsGridComponent from "../../../components/top-bar/ActionColumnsGrid.component.tsx"
+import ActionRefreshDataComponent from "../../../components/top-bar/ActionRefreshData.component.tsx"
+
+const UnitTopBarComponent = () => {
+   const [gridColumnsVisible, setGridColumnsVisible] = useAtom(
+      unitGridColumnsVisibleAtom,
+   )
+   const [selectedUnit, setSelectedUnit] = useAtom(selectedUnitAtom)
+   const setOpenDeleteModal = useSetAtom(openUnitDeleteModalAtom)
+   const navigate = useNavigate()
+
+   const onActionCreateUnit = useCallback(() => {
+      setSelectedUnit({})
+      navigate("/units/create")
+   }, [navigate, setSelectedUnit])
+
+   return (
+      <Group position="apart">
+         <Group>
+            <ActionCreateComponent createFunction={onActionCreateUnit} />
+            <ActionCloneComponent
+               disabled={!(selectedUnit?.name !== undefined)}
+               cloneUrl={"/units/create"}
+            />
+            <ActionEditComponent
+               editUrl={"/units/update/" + selectedUnit?.unitId}
+               disabled={!(selectedUnit?.name !== undefined)}
+            />
+            <ActionDeleteComponent
+               deleteFunction={setOpenDeleteModal}
+               disabled={!(selectedUnit?.name !== undefined)}
+            />
+            <SearchUnitByColumnComponent />
+            <ActionExportComponent
+               pdfUrl={"unit/download-unit-pdf"}
+               pdfName="units.pdf"
+               excelUrl={"unit/download-unit-excel"}
+               excelName="units.xlsx"
+            />
+            <ActionColumnsGridComponent>
+               <Checkbox.Group
+                  value={gridColumnsVisible}
+                  onChange={setGridColumnsVisible}
+                  w={200}
+               >
+                  <Group mt="xs" px="xs" pb="xs">
+                     <Checkbox value="name" label="Nombre" />
+                     <Checkbox value="description" label="Descripcion" />
+                     <Checkbox value="code" label="Code" />
+                  </Group>
+               </Checkbox.Group>
+            </ActionColumnsGridComponent>
+         </Group>
+         <ActionRefreshDataComponent queryKey={["/api/unit/units"]} />
+      </Group>
+   )
+}
+
+export default UnitTopBarComponent
