82fafdb86f35f5e8780c3347ad51730713e6e2bd
enano17711 <enano17711@gmail.com>
2023-10-12 22:17:18 -0400

    supplier main view ok. Suppliers.view.tsx

--- src/views/suppliers/Suppliers.view.tsx
+++ src/views/suppliers/Suppliers.view.tsx
@@ -1,45 +1,26 @@
-import CustomBreadcrumbsComponent from "../../components/CustomBreadcrumbs.component.tsx"
+import { useSetAtom } from "jotai"
+import { selectedSupplierAtom } from "../../store/supplier.atoms.ts"
 import React, { useEffect } from "react"
-import { Button, Group, Space } from "@mantine/core"
-import { IconPlus } from "@tabler/icons-react"
-import { ModalCreateSupplierComponent } from "./components/ModalCreateSupplier.component.tsx"
-import { useAppStore } from "../../store"
-import DialogDeleteSupplierComponent from "./components/DialogDeleteSupplier.component.tsx"
-import { useElementSize } from "@mantine/hooks"
-import { DataGridSupplierComponent } from "./components/DataGridSupplier.component.tsx"
+import { Can } from "../../access-control.ts"
+import SupplierTopBarComponent from "../suppliers/components/SupplierTopBar.component.tsx"
+import { Space } from "@mantine/core"
+import { DataGridSupplierComponent } from "../suppliers/components/DataGridSupplier.component.tsx"
+import DialogDeleteSupplierComponent from "../suppliers/components/DialogDeleteSupplier.component.tsx"
 
-const routes = [
-   { path: "/", title: "Inicio" },
-   { path: "/suppliers", title: "Proveedores" },
-]
 const SuppliersView = () => {
-   const { suppliersStore } = useAppStore()
-   const { suppliers } = suppliersStore.getters
-
-   const { ref, width, height } = useElementSize()
+   const setSingleSupplier = useSetAtom(selectedSupplierAtom)
 
    useEffect(() => {
-      if (suppliers.length === 0) {
-         suppliersStore.actions.loadSuppliers()
-      }
+      setSingleSupplier({})
    }, [])
 
    return (
-      <>
-         <Group position="apart" ref={ref}>
-            <CustomBreadcrumbsComponent routes={routes} />
-            <Button
-               leftIcon={<IconPlus size={20} />}
-               onClick={() => suppliersStore.actions.prepareForCreate()}
-            >
-               Crear Proveedor
-            </Button>
-         </Group>
+      <Can I="ViewSupplier" a="user">
+         <SupplierTopBarComponent />
          <Space h="sm" />
-         <DataGridSupplierComponent dataSource={suppliers} />
-         <ModalCreateSupplierComponent />
+         <DataGridSupplierComponent />
          <DialogDeleteSupplierComponent />
-      </>
+      </Can>
    )
 }
 
