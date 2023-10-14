1048d2b7e2d18f60eb3eb1505c5277a36f90e2ac
enano17711 <enano17711@gmail.com>
2023-10-13 23:54:13 -0400

    item main view ok. Items.view.tsx

--- src/views/items/Items.view.tsx
+++ src/views/items/Items.view.tsx
@@ -1,79 +1,25 @@
-import CustomBreadcrumbsComponent from "../../components/CustomBreadcrumbs.component.tsx"
-import { Button, Group, Space } from "@mantine/core"
-import { IconCsv, IconPdf, IconPlus } from "@tabler/icons-react"
-import { ModalCreateItemComponent } from "./components/ModalCreateItem.component.tsx"
-import { DataGridItemComponent } from "./components/DataGridItem.component.tsx"
-import DialogDeleteItemComponent from "./components/DialogDeleteItem.component.tsx"
-import ModalCreateItemBatchComponent from "./components/ModalCreateItemBatch.component.tsx"
-import DialogDeleteItemBatchComponent from "./components/DialogDeleteItemBatch.component.tsx"
+import { useSetAtom } from "jotai"
+import { selectedItemAtom } from "../../store/item.atoms.ts"
+import React, { useEffect } from "react"
 import { Can } from "../../access-control.ts"
-import React from "react"
-import { accessTokenKey, refreshAccessTokenKey } from "../../axios-utils.ts"
+import ItemTopBarComponent from "../items/components/ItemTopBar.component.tsx"
+import { Space } from "@mantine/core"
+import DataGridItemComponent from "../items/components/DataGridItem.component.tsx"
+import DialogDeleteItemComponent from "../items/components/DialogDeleteItem.component.tsx"
 
-const routes = [
-   { path: "/", title: "Inicio" },
-   { path: "/items", title: "Items" },
-]
 const ItemsView = () => {
-   const exportCsv = () => {
-      fetch("https://localhost:5001/api/brand/download-brand-excel", {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/csv",
-         },
-      })
-         .then((res) => res.blob())
-         .then((blob) => {
-            const url = window.URL.createObjectURL(blob)
-            const a = document.createElement("a")
-            a.style.display = "none"
-            a.href = url
-            a.download = "brand.csv"
-            document.body.appendChild(a)
-            a.click()
-            window.URL.revokeObjectURL(url)
-         })
-   }
-   const exportPdf = () => {
-      const userData = window.localStorage.getItem("userDataSession")
-      const userId = JSON.parse(userData!).userId
+   const setSingleItem = useSetAtom(selectedItemAtom)
 
-      fetch("https://localhost:5001/api/brand/download-brand-pdf/" + userId, {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/pdf",
-         },
-      })
-         .then((res) => res.blob())
-         .then((blob) => {
-            const url = window.URL.createObjectURL(blob)
-            const a = document.createElement("a")
-            a.style.display = "none"
-            a.href = url
-            a.download = "brand.pdf"
-            document.body.appendChild(a)
-            a.click()
-            window.URL.revokeObjectURL(url)
-         })
-   }
+   useEffect(() => {
+      setSingleItem({})
+   }, [])
 
    return (
       <Can I="ViewItem" a="user">
-         <Group position="apart">
-            <CustomBreadcrumbsComponent routes={routes} />
-            <Group id="item-header">
-               <Button leftIcon={<IconPdf />} onClick={exportPdf}>
-                  Export
-               </Button>
-               <Button leftIcon={<IconCsv />} onClick={exportCsv}>
-                  Export
-               </Button>
-            </Group>
-         </Group>
+         <ItemTopBarComponent />
          <Space h="sm" />
          <DataGridItemComponent />
-         <ModalCreateItemBatchComponent />
-         <DialogDeleteItemBatchComponent />
+         <DialogDeleteItemComponent />
       </Can>
    )
 }
